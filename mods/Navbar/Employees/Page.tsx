3823721ffd782a8eec234fb06f7a8ac357eddd4d
FiRe <levicowan2005@icloud.com>
2022-04-01 15:33:22 -0500

    Make employees menu more rounded

--- mods/Navbar/Employees/Page.tsx
+++ mods/Navbar/Employees/Page.tsx
@@ -2,9 +2,9 @@ import Link from 'next/link'
 
 let EmployeesPage = props => {
   return <>
-    {props.bool ? <div className="flex flex-col fixed z-30 w-32 bg-black text-gray-200 top-18 rounded shadow select-none" style={{left: "14.78rem"}}>
+    {props.bool ? <div className="flex flex-col fixed z-30 w-32 bg-black text-gray-200 top-18 rounded-lg shadow select-none" style={{left: "14.78rem"}}>
       <Link href="/employees/filter/developers">
-        <a className="rounded-t hover:bg-stone-900 py-2 text-center">Developers</a>
+        <a className="rounded-t-lg hover:bg-stone-900 py-2 text-center">Developers</a>
       </Link>
       <Link href="/employees/filter/designers">
         <a className="hover:bg-stone-900 text-sm py-2 text-center">Graphic Designers</a>
@@ -16,7 +16,7 @@ let EmployeesPage = props => {
         <a className="hover:bg-stone-900 py-2 text-center">Writers</a>
       </Link>
       <Link href="/employees">
-        <a className="rounded-b hover:bg-stone-900 py-2 text-center">See all</a>
+        <a className="rounded-b-lg hover:bg-stone-900 py-2 text-center">See all</a>
       </Link>
     </div>: <></>}
     {props.bool ? <div className="flex z-20 fixed top-0 right-0 left-0 bottom-0" onClick={()=>{props.set(false)}}></div>: <></>}
