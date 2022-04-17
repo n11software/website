0034c4aee26b999a181999c29ffc2ff60d025a24
FiRe <levih@n11software.com>
2022-04-17 17:26:16 -0500

    Fix odd side scroll thing

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,18 +8,18 @@ let Index = () => {
       <title>N11</title>
     </Head>
     <Navbar/>
-    <div className="w-screen h-screen flex">
+    <div className="w-full h-screen flex">
       <div className="flex flex-col text-stone-800 w-full pt-32 pl-24">
         <span className="text-5xl font-black mb-2">Optimized, secure, and modern products.</span>
         <span className="text-2xl w-3/4 text-stone-600 mb-12">N11 is a business focused on creating things from nothing making them secure, fast, and modernized.</span>
         <ApplyButton/>
       </div>
       <div className="flex flex-col text-white w-full items-end justify-center pr-24 pt-32 pb-12">
-        <img src="/HeroNuclear.png" className="h-full w-full"/>
+        <img src="/HeroNuclear.png" className="h-full w-full object-scale-down"/>
       </div>
     </div>
     <img src="Waves/White-Black.svg" className="w-full h-48"/>
-    <div className="w-screen flex flex-col bg-black h-screen">
+    <div className="w-full flex flex-col bg-black h-screen">
       <span className="text-5xl font-black text-white pl-32 pt-20"></span>
     </div>
     <img src="Waves/Black-White.svg" className="w-full h-48"/>
