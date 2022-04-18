54c750b6f2456c2ca97b06340cdd503cee914306
FiRe <levicowan2005@icloud.com>
2022-04-18 15:24:50 -0500

    Add responsive mobile design

--- mods/Navbar/Navbar.tsx
+++ mods/Navbar/Navbar.tsx
@@ -9,11 +9,13 @@ import EmployeesPage from './Employees/Page'
 import ContactButton from './Contact/Button'
 import ApplyButton from './Apply/Button'
 import DonateButton from './Donate/Button'
+import { MenuIcon } from '@heroicons/react/solid'
 
 let Navbar = props => {
   let [isProductsOpen, setIsProductsOpen] = useState(false)
   let [isEmployeesOpen, setIsEmployeesOpen] = useState(false)
   let [isScroll, setIsScroll] = useState(false)
+  let [isMenuOpen, setIsMenuOpen] = useState(false)
 
   let SetProducts = isActive => {
     setIsProductsOpen(isActive);
@@ -36,9 +38,10 @@ let Navbar = props => {
   useEffect(() => {
     document.body.onscroll = () => {
       if (window.scrollY > 16) {
-        SetScroll(true);
+        SetScroll(true)
+        setIsMenuOpen(false)
       } else {
-        SetScroll(false);
+        SetScroll(false)
       }
     }
   }, [])
@@ -47,14 +50,15 @@ let Navbar = props => {
     <div className={`flex z-10 py-2 px-4 bg-white select-none shadow fixed items-center transition-all ${isScroll ? "top-4 right-4 left-4 rounded-xl": "top-0 left-0 right-0 rounded-none"}`} id="navbar">
       <div className="flex w-full">
         <Link href="/">
-          <a><Logo/></a>
+          <a className="sm:block hidden"><Logo/></a>
         </Link>
-        <div className="pl-12 space-x-4 flex w-full">
+        <a className={`sm:hidden flex cursor-pointer space-x-2 items-center px-2 py-2 rounded-lg transition-colors ${isMenuOpen ? "bg-black text-gray-200": "text-gray-800 hover:bg-stone-300"}`} onClick={() => setIsMenuOpen(!isMenuOpen)}><MenuIcon className="w-6"/></a>
+        {!isMenuOpen ? <div className="sm:flex hidden pl-12 space-x-4 w-full">
           <ProductsButton bool={isProductsOpen} set={SetProducts}/>
           <EmployeesButton bool={isEmployeesOpen} set={SetEmployees}/>
           <DonateButton/>
           <ContactButton/>
-        </div>
+        </div>: <></>}
       </div>
       <div className="flex justify-end text-right w-full">
         <ApplyButton/>
@@ -62,6 +66,12 @@ let Navbar = props => {
     </div>
     <ProductsPage bool={isProductsOpen}/>
     <EmployeesPage bool={isEmployeesOpen}/>
+    {isMenuOpen ? <div className="sm:hidden bg-white flex flex-col space-y-4 w-full h-screen px-4 pt-16 fixed top-0 left-0 right-0 bottom-0 z-0">
+      <ProductsButton/>
+      <EmployeesButton/>
+      <DonateButton/>
+      <ContactButton/>
+    </div>: <></>}
   </>
 }
 
--- mods/Navbar/Products/Button.tsx
+++ mods/Navbar/Products/Button.tsx
@@ -1,9 +1,14 @@
 import { ChevronDownIcon } from '@heroicons/react/solid'
+import Link from 'next/link'
 
 let ProductsButton = props => {
-  return <div className={`flex cursor-pointer space-x-2 items-center py-2 px-2 rounded-lg transition-colors ${props.bool ? "bg-black text-gray-200": "text-gray-800 hover:bg-stone-300"}`} onClick={()=>props.set(!props.bool)}>
+  return props.set !== undefined ? <div className={`flex cursor-pointer space-x-2 items-center py-2 px-2 rounded-lg transition-colors ${props.bool ? "bg-black text-gray-200": "text-gray-800 hover:bg-stone-300"}`} onClick={()=>props.set(!props.bool)}>
     <span>Products</span>
     <ChevronDownIcon className={`h-4 w-4 transition-transform ease-in-out ${props.bool ? "rotate-180": ""}`} />
-  </div>
+  </div>: <Link href="/products">
+    <a className="flex cursor-pointer justify-center space-x-2 items-center py-2 px-2 rounded-lg transition-colors active:bg-black active:text-gray-200 text-gray-800 hover:bg-stone-300">
+      Products
+    </a>
+  </Link>
 }
 export default ProductsButton
