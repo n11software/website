3342c5ba28dfb8124486cf4763def34ec80aba47
FiRe <levih@n11.dev>
2022-09-11 05:44:15 -0500

    Engine: made delay only for host not url
    Signed-off-by: FiRe <levih@n11.dev>

--- Engine/Engine.js
+++ Engine/Engine.js
@@ -37,13 +37,13 @@ let PushSite = (url, title, description, keywords) => {
     })
 }
 let urls = []
-let prevURL = ""
+let prevHost = ""
 let Engine = async url => {
-    if (prevURL == url) execSync("sleep 0.15")
-    prevURL = url
     if (!/^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(url)) return
     let proto = url.split(':')[0]
     let host = url.split('/')[2]
+    if (prevHost == host) execSync("sleep 0.15")
+    prevHost = host
     return fetch(url, {headers: {
         'Accept-Language': 'en;q=0.9, *;q=0.8',
     }}).then(response => {
@@ -122,6 +122,6 @@ let cycle = () => {
     })
 }
 
-Engine('http://biglistofwebsites.com/').then(()=>{
+Engine('https://rawranked.com/').then(()=>{
     cycle()
 })
