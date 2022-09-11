3b24649bda260fc7080cd5d94a139d420bbf9ab8
FiRe <levih@n11.dev>
2022-09-11 05:38:10 -0500

    Engine: fixed delay and status
    Signed-off-by: FiRe <levih@n11.dev>

--- Engine/Engine.js
+++ Engine/Engine.js
@@ -37,8 +37,10 @@ let PushSite = (url, title, description, keywords) => {
     })
 }
 let urls = []
+let prevURL = ""
 let Engine = async url => {
-    execSync("sleep 0.25")
+    if (prevURL == url) execSync("sleep 0.15")
+    prevURL = url
     if (!/^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(url)) return
     let proto = url.split(':')[0]
     let host = url.split('/')[2]
@@ -70,7 +72,7 @@ let Engine = async url => {
             }
             if (urel != null) urel.forEach(url => urls.push(proto + "://" + host + url[url.length-1] == "/"? "": "/" + url))
 
-            if (response.ok && response.headers.get('Content-Type').includes("text/html")) {
+            if (response.status == 200 && response.headers.get('Content-Type').includes("text/html")) {
                 try {
                     let dom = parser.parseFromString(text)
                     if (dom.getElementsByTagName('title').length > 0) {
