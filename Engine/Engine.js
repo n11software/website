6c76fbcedbe6304e76b1c17dff1cf8e397f2dfa8
FiRe <levih@n11.dev>
2022-09-11 02:26:44 -0500

    Engine: fix duplicates
    Signed-off-by: FiRe <levih@n11.dev>

--- Engine/Engine.js
+++ Engine/Engine.js
@@ -2,11 +2,12 @@
 let DomParser = require('dom-parser')
 let parser = new DomParser()
 let mysql = require('mysql')
+require('dotenv').config()
 
 var sql = mysql.createConnection({
     host: "n11.dev",
-    user: "",
-    password: "",
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD,
     database: "engine"
 })
 
@@ -14,6 +15,21 @@ sql.connect(err => {
     if (err) throw err
     console.log("Connected to database!")
 })
+
+let push = (url, title, description) => {
+    sql.query("SELECT * FROM entry WHERE url = ?", [url], function (err, result) {
+        if (err) throw err
+        console.log(result)
+        if (result.length == 0) {
+            sql.query("INSERT INTO entry (url, title, description) VALUES (?, ?, ?)", [url, title, description], function (err, result) {
+                if (err) throw err
+                console.log("1 record inserted")
+            })
+        } else {
+            console.log("Entry already exists")
+        }
+    })
+}
 
 let Engine = async url => {
     if (!/^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(url)) return
@@ -67,13 +83,10 @@ let Engine = async url => {
             console.log(title)
             console.log(description)
             console.log(urls)
-            sql.query("INSERT INTO entry (url, title, description) VALUES ('" + url.replace(/'/g, '\\\'') + "', '" + title.replace(/'/g, '\\\'') + "', '" + description.replace(/'/g, '\\\'') + "')", function (err, result) {
-                if (err) throw err
-                console.log("1 record inserted")
-            })
+            push(url, title, description)
             // if (urls != null) urls.forEach(url => await Engine(url))
         })
     }).catch(()=>{console.log("failed...")})
 }
 
-Engine('https://google.com')
+Engine('https://wikipedia.org')
