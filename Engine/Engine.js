0821fa0f54c80fc687039e0ec79d5c2dd5d3d1af
FiRe <levih@n11.dev>
2022-09-11 08:05:29 -0500

    Engine: modified rate and lock
    Signed-off-by: FiRe <levih@n11.dev>

--- Engine/Engine.js
+++ Engine/Engine.js
@@ -83,14 +83,12 @@ let PushSite = (url, title, description, keywords) => {
         }
     })
 }
+let recents = []
 let urls = []
-let prevHost = ""
 let Engine = async url => {
     if (!/^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(url)) return
     let proto = url.split(':')[0]
     let host = url.split('/')[2]
-    if (prevHost == host) execSync("sleep 0.15")
-    prevHost = host
     return fetchUnlessMassive(url).then(text => {
         if (text == null || r == undefined || r == null) return
         let title = "", description = "", keywords
@@ -157,8 +155,6 @@ let Engine = async url => {
         return 1
     })
 }
-
-let recents = []
 
 let cycle = () => {
     if (!recents.includes(urls[0])) {
