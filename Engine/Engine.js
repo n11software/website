7a01a00868f0bbd21a6645fda4ebf80b06dcfc85
FiRe <levih@n11.dev>
2022-09-11 03:27:27 -0500

    Engine: added cycling
    Signed-off-by: FiRe <levih@n11.dev>
    
    We now cycle instead of going straight to the new url

--- Engine/Engine.js
+++ Engine/Engine.js
@@ -1,5 +1,6 @@
 // Run with node.js
 let DomParser = require('dom-parser')
+const {execSync} = require('child_process')
 let parser = new DomParser()
 let mysql = require('mysql')
 require('dotenv').config()
@@ -21,26 +22,25 @@ let push = (url, title, description) => {
         if (err) throw err
         if (result.length == 0) {
             sql.query("INSERT INTO entry (url, title, description) VALUES (?, ?, ?)", [url, title, description], function (err, result) {
-                if (err) throw err
-                console.log("Added " + url + " to database!")
+                if (err) console.log(err)
+                console.log("\x1b[32mAdded " + url + " to database!\x1b[0m")
             })
-            sql.commit()
         } else {
-            console.log(url + " already exists!")
+            console.log("\x1b[34m" + url + " already exists!\x1b[0m")
         }
     })
-    sql.commit()
 }
-
+let urls = []
 let Engine = async url => {
+    execSync("sleep 0.`25")
     if (!/^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(url)) return
     console.log("Parsing " + url)
     let proto = url.split(':')[0]
     let host = url.split('/')[2]
-    fetch(url).then(response => {
-        response.text().then(text => {
+    return fetch(url).then(response => {
+        return response.text().then(text => {
             if (text == null) return
-            let title = "", description = "", urls = []
+            let title = "", description = ""
             const urlRegDQ = /"(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?"/gi
             const urlRegSQ = /'(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?'/gi
             const urlRegGR = /`(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?`/gi;
@@ -85,12 +85,19 @@ let Engine = async url => {
                     console.log(e)
                 }
             }
-            console.log(title)
-            console.log(description)
             push(url, title, description)
-            // if (urls != null) urls.forEach(url => await Engine(url))
+            return 1
         })
-    }).catch(()=>{console.log("failed...")})
+    }).catch(()=>{console.log("\x1b[31mNot a valid URL!\x1b[0m")})
 }
 
-Engine('https://github.com')
+let cycle = () => {
+    Engine(urls[0]).then(()=>{
+        urls.shift()
+        cycle()
+    })
+}
+
+Engine('https://yahoo.com').then(()=>{
+    cycle()
+})
