f754b10769e226d3a217acfccb88c0b669e2da72
FiRe <levih@n11.dev>
2022-09-17 02:29:54 -0500

    Website: added gzip compression and locally hosted fonts
    Signed-off-by: FiRe <levih@n11.dev>

--- Website/Source/main.cpp
+++ Website/Source/main.cpp
@@ -1,4 +1,23 @@
 #include <link>
+#include <iostream>
+#include "../Include/zstr.hpp"
+
+std::string compress(std::string data) {
+  std::ostringstream output;
+  std::istringstream ds(data);
+
+  zstr::ostream zs(output);
+  const std::streamsize size = 1 << 16;
+  char* buf = new char[size];
+  while (true) {
+    ds.read(buf, size);
+    std::streamsize count = ds.gcount();
+    if (count == 0) break;
+    zs.write(buf, count);
+  }
+  delete [] buf;
+  return (std::ostringstream&)zs << std::flush, output.str();
+}
 
 int main() {
   Link Server(3000);
@@ -17,24 +36,31 @@ int main() {
   // Return Index Page
   Server.Get("/", [](Request* req, Response* res) {
     res->SetHeader("Content-Type", "text/html; charset=utf-8");
-    res->SendFile("www/index.html");
+    res->SetHeader("Content-Encoding", "gzip");
+    std::ifstream file("www/index.html");
+    std::string str((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    std::string compressed = compress(str);
+    res->Send(compressed);
   });
 
   // Return stylesheet for index
   Server.Get("/css/index.css", [](Request* req, Response* res) {
     res->SetHeader("Content-Type", "text/css; charset=utf-8");
-    res->SendFile("www/css/index.css");
+    res->SetHeader("Content-Encoding", "gzip");
+    std::ifstream file("www/css/index.css");
+    std::string str((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    std::string compressed = compress(str);
+    res->Send(compressed);
   });
 
   // Return javascript for index
   Server.Get("/js/index.js", [](Request* req, Response* res) {
     res->SetHeader("Content-Type", "text/javascript; charset=utf-8");
-    res->SendFile("www/js/index.js");
-  });
-
-  // Return font for logo
-  Server.Get("/fonts/saldasoftheavy.otf", [](Request* req, Response* res) {
-    res->SendFile("www/fonts/saldasoftheavy.otf");
+    res->SetHeader("Content-Encoding", "gzip");
+    std::ifstream file("www/js/index.js");
+    std::string str((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    std::string compressed = compress(str);
+    res->Send(compressed);
   });
 
   // Search Page
@@ -43,12 +69,39 @@ int main() {
       res->Error(404);
     } else {
       res->SetHeader("Content-Type", "text/html; charset=utf-8");
+      res->SetHeader("Content-Encoding", "gzip");
       if (req->GetQuery("suggestions")=="true") {
-        res->Send("{\"suggestions\":[ \"Hi\", \"Hello\", \"Bye\" ]}");
+        std::string compressed = compress("{\"suggestions\":[ \"Hi\", \"Hello\", \"Bye\" ]}");
+        res->Send(compressed);
       } else {
-        res->SendFile("www/search.html");
+        std::ifstream file("www/search.html");
+        std::string str((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+        std::string compressed = compress(str);
+        res->Send(compressed);
       }
     }
+  });
+
+
+
+
+
+  // Fonts
+  Server.Get("/fonts/jbm.css", [](Request* req, Response* res) {
+    res->SetHeader("Content-Type", "text/css; charset=utf-8");
+    res->SetHeader("Content-Encoding", "gzip");
+    res->SetHeader("Cache-Control", "private, max-age=86400, stale-while-revalidate=604800");
+    std::ifstream file("www/fonts/jbm.css");
+    std::string str((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    std::string compressed = compress(str);
+    res->Send(compressed);
+  });
+
+  Server.Get("/fonts/jbm.woff2", [](Request* req, Response* res) {
+    res->SetHeader("Content-Type", "font/woff2");
+    res->SetHeader("Cache-Control", "public, max-age=31536000");
+    res->SetHeader("Accept-Ranges", "bytes");
+    res->SendFile("www/fonts/jbm.woff2");
   });
 
   std::cout << "Server running on port 3000" << std::endl;
