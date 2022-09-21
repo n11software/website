4593b8a9991a75b167d647289e1a273bf3983fb6
FiRe <levih@n11.dev>
2022-09-21 18:33:46 -0500

    Update: better update.sh
    Signed-off-by: FiRe <levih@n11.dev>

--- update.sh
+++ update.sh
@@ -1,6 +1,11 @@
+sed -i -e 's/3000/3001/g' Website/Source/main.cpp
+systemctl stop n11.dev
+cmake -GNinja .
+ninja
 rm -r /web/n11.dev/
 mkdir /web/n11.dev/
 cp Website/website /web/n11.dev/Link
 cp -R www /web/n11.dev/www
 cp db.txt /web/n11.dev/db.txt
 chown -R web:web /web/n11.dev/
+systemctl start n11.dev
