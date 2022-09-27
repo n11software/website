847ff461e722444c36be2ade199cad2408d720e0
FiRe <levih@n11.dev>
2022-09-27 00:07:20 -0500

    Engine: using puppeteer and checking robots.txt
    Signed-off-by: FiRe <levih@n11.dev>

--- Engine/Engine.js
+++ Engine/Engine.js
@@ -1,205 +1,101 @@
-// Run with node.js
-let DomParser = require('dom-parser')
-const {execSync} = require('child_process')
-let parser = new DomParser()
-let mysql = require('mysql')
-let crypto = require('crypto')
-require('dotenv').config()
+const { expect } = require('chai')
+let puppeteer = require('puppeteer')
 
-var sql = mysql.createConnection({
-    host: "n11.dev",
-    user: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    database: "n11",
-    charset : 'utf8mb4'
-})
+let score = 0
 
-sql.connect(err => {
-    if (err) throw err
-    console.log("Ready...")
-})
+let proto, host, path
+let disallowed = [], allowed = []
+let sitemap = []
+let hasRobots = false
 
-let r
-async function* iterator(url) {
-    try {
-        const utf8Decoder = new TextDecoder('utf-8');
-        r = await fetch(url, {headers: {
-            'Accept-Language': 'en;q=0.9, *;q=0.8',
-        }}).catch((e)=>{
-            process.stdout.clearLine(0)
-            process.stdout.cursorTo(0)
-            process.stdout.write("\x1b[31m" + url + "\x1b[0m")
-        })
-        const reader = r.body.getReader();
-        let { value: chunk, done: readerDone } = await reader.read();
-        chunk = chunk ? utf8Decoder.decode(chunk) : '';
-    
-        const re = /\n|\r|\r\n/gm;
-        let startIndex = 0;
-        let result;
-    
-        while (true) {
-            let result = re.exec(chunk);
-            if (!result) {
-                if (readerDone) break;
-                let remainder = chunk.substr(startIndex);
-                ({ value: chunk, done: readerDone } = await reader.read());
-                chunk = remainder + (chunk ? utf8Decoder.decode(chunk) : '');
-                startIndex = re.lastIndex = 0;
-                process.stdout.cursorTo(0)
-                process.stdout.write("\x1b[36m"+urls[0]+"\x1b[0m (" + chunk.length + ")")
-                continue;
-            }
-            process.stdout.clearLine(0)
-            process.stdout.cursorTo(0)
-            process.stdout.write("\x1b[36m"+urls[0]+"\x1b[0m")
-            yield chunk.substring(startIndex, result.index);
-            startIndex = re.lastIndex;
-        }
-    
-        if (startIndex < chunk.length) {
-            process.stdout.clearLine(0)
-            process.stdout.cursorTo(0)
-            process.stdout.write("\x1b[36m"+urls[0]+"\x1b[0m")
-            yield chunk.substr(startIndex);
-        }
-    } catch (e) {
-        yield null
-    }
-}
-  
-  async function fetchUnlessMassive(url) {
-    let ret = ""
-    for await (const line of iterator(url)) {
-        ret += line
-        if (ret.length>1048576) return ret
-    }
-    return ret
-  }
-  
+let links = []
 
-let PushSite = (url, title, description, keywords, lang) => {
-    let hash = crypto.createHash('sha256').update(url).digest('hex')
-    let time = new Date().toISOString()
-    sql.query("SELECT * FROM sites WHERE hash = ?", [hash], function (err, result) {
-        if (err) throw err
-        if (result.length == 0) {
-            sql.query("INSERT INTO sites (hash, url, title, description, keywords, lang, time, updated) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", [hash, url, title, description, keywords, lang, time, time], function (err, result) {
-                if (err) console.log(err)
-                console.log("\x1b[32m" + url + "\x1b[0m")
-                process.stdout.clearLine(0)
-                process.stdout.cursorTo(0)
-                process.stdout.write("\x1b[32m" + url + "\x1b[0m\n")
-            })
-        } else {
-            sql.query("UPDATE sites SET title = ?, description = ?, keywords = ?, lang = ?, updated = ? WHERE hash = ?", [title, description, keywords, lang, time, hash], (err, res) => {
-                if (err) console.log(err)
-                process.stdout.clearLine(0)
-                process.stdout.cursorTo(0)
-                process.stdout.write("\x1b[34m" + url + "\x1b[0m\n")
-            })
-        }
-    })
-}
-let recents = []
-let urls = []
-let Engine = async url => {
-    if (!/^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(url)) return 2
-    let proto = url.split(':')[0]
-    let host = url.split('/')[2]
-    return fetchUnlessMassive(url).then(text => {
-        if (text == null || r == undefined || r == null) return
-        let title = "", description = "", keywords
-        const urlRegDQ = /"(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?"/gi
-        const urlRegSQ = /'(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?'/gi
-        const urlRegGR = /`(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?`/gi;
-        let udq = text.match(urlRegDQ)
-        let usq = text.match(urlRegSQ)
-        let ugr = text.match(urlRegGR)
-        if (udq != null) urls = urls.concat(udq)
-        if (usq != null) urls = urls.concat(usq)
-        if (ugr != null) urls = urls.concat(ugr)
-        for (let i = 0; i < urls.length; i++) {
-            urls[i] = urls[i].replace(/"/g, '').replace(/'/g, '').replace(/`/g, '')
-            if (urls[i].startsWith('//')) urls[i] = proto + ':' + urls[i]
-            if (urls[i].startsWith('/')) urls[i] = proto + '://' + host + urls[i]
-        }
-
-        const urlRegRel = /<a\s+(?:[^>]*?\s+)?href=(["'])((?!(?:(?:(?:https?|ftp):)?\/\/)).*?)\1/gi
-        let urel = [], match;
-        while (match = urlRegRel.exec(text)) {
-            urel.push(match[2]);
-        }
-        if (urel != null) urel.forEach(url => urls.push(proto + "://" + host + url[url.length-1] == "/"? "": "/" + url))
-
-        let contentType = r.headers.get('Content-Type')
-        let lang = r.headers.get('Content-Language')
-        if (r.status == 200 && contentType != null && contentType.includes('text/html')) {
-            try {
-                let dom = parser.parseFromString(text)
-                if (dom.getElementsByTagName('title').length > 0) {
-                    title = dom.getElementsByTagName("title")[0].textContent
-                } else if (dom.getElementsByTagName('h1').length > 0) {
-                    title = dom.getElementsByTagName("h1")[0].textContent
-                } else if (dom.getElementsByTagName('h2').length > 0) {
-                    title = dom.getElementsByTagName("h2")[0].textContent
-                } else if (dom.getElementsByTagName('h3').length > 0) {
-                    title = dom.getElementsByTagName("h3")[0].textContent
-                } else if (dom.getElementsByTagName('h4').length > 0) {
-                    title = dom.getElementsByTagName("h4")[0].textContent
-                } else if (dom.getElementsByTagName('h5').length > 0) {
-                    title = dom.getElementsByTagName("h5")[0].textContent
-                } else if (dom.getElementsByTagName('h6').length > 0) {
-                    title = dom.getElementsByTagName("h6")[0].textContent
-                }
-                if (dom.getElementsByName("description").length > 0) {
-                    description = dom.getElementsByName("description")[0].getAttribute("content").substring(0, 256)
-                } else if (dom.getElementsByTagName("p").length > 0) {
-                    let longest = ""
-                    dom.getElementsByTagName("p").forEach(text => {
-                        if (text.textContent.length <= 256 && text.textContent.length > longest.length) {
-                            longest = text.textContent
+let engine = async () => {
+    let browser = await puppeteer.launch({headless: false, args: [ '--window-size=1920,1080' ]})
+    let robots = async () => {
+        await fetch(proto + "://" + host + '/robots.txt').then(async res=>{
+            if (await res.status==200) {
+                hasRobots = true
+                let page = await browser.newPage()
+                page.setViewport({ width: 1920, height: 1080 })
+                page.setUserAgent("N11")
+                await page.goto(proto + "://" + host + '/robots.txt')
+                let robotstxt = await page.evaluate(() => document.body.innerText)
+                
+                let robots = robotstxt.split('\n')
+                let ua = []
+                let ll
+                // NOTE FOR LATER:
+                // Add wildcard support and $ support
+                for (line in robots) {
+                    if (robots[line].split(':')[0].toLowerCase() == 'user-agent') {
+                        if (ll == 'user-agent') ua.push(robots[line].split(':')[1].toLowerCase().trim())
+                        else ua = [ robots[line].split(':')[1].toLowerCase().trim() ]
+                    }
+                    if (ua.includes('n11') || ua.includes('*') || ua == []) {
+                        if (robots[line].split(':')[0].toLowerCase() == 'disallow') {
+                            disallowed.push(robots[line].split(':')[1].toLowerCase().trim())
+                        } else if (robots[line].split(':')[0].toLowerCase() == 'allow') {
+                            allowed.push(robots[line].split(':')[1].toLowerCase().trim())
                         }
-                    })
-                    description = longest
+                    }
+                    if (robots[line].split(':')[0].toLowerCase() == 'sitemap') {
+                        sitemap.push(robots[line].substring(robots[line].indexOf(':')+1).toLowerCase().trim())
+                    }
+                    ll = robots[line].split(':')[0].toLowerCase()
                 }
-                if (dom.getElementsByName("keywords").length > 0) {
-                    keywords = dom.getElementsByName("keywords")[0].getAttribute("content")
-                }
-                if (dom.getElementsByName("html").length > 0) {
-                    lang = dom.getElementsByName("html")[0].getAttribute("lang")
-                }
-            } catch (e) {
-                console.log(e)
+                page.close()
             }
-            if (title == "") title = url
-            PushSite(url, title, description, keywords, lang == null? "": lang)
-        }
-        return 1
-    })
-}
-
-let cycle = () => {
-    if (urls.length == 0) {
-        console.log("Done")
-        sql.end()
-        return
-    }
-    if (!recents.includes(urls[0])) {
-        console.log("\x1b[36m"+urls[0]+"\x1b[0m")
-        Engine(urls[0]).then(ret=>{
-            if (ret == 2 || ret == null) console.log("\n")
-            recents.push(urls[0])
-            if (recents.length > 100000) recents.shift()
-            urls.shift()
-            cycle()
         })
-    } else {
-        console.log("\x1b[35m"+urls[0]+"\x1b[0m")
-        urls.shift()
-        cycle()
     }
+
+    let crawler = async (url) => {
+        if (host != url.split('/')[2]) return // Not owned by host (Add referer score later)
+        path = url.substring(proto.length + 3 + host.length)
+        browser.pages().then(pages => {
+            pages[0].close()
+        })
+        let page = await browser.newPage()
+        page.setViewport({ width: 1920, height: 1080 })
+        page.setUserAgent("N11")
+        await page.goto(url)
+        let ttl = await page.metrics()
+        if (await ttl.TaskDuration <= 1.8) score += 1
+        else if (await ttl.TaskDuration <= 3) score += 0.5
+
+        await page.$$('a').then(async e => {
+            for (link in e) {
+                let href = await (await (e[link].evaluate(el => el.href))).trim()
+                if (href.startsWith('//')) href = proto + ':' + href
+                else if (href.startsWith('/')) href = proto + '://' + host + href
+                else if (!href.startsWith('http')) href = proto + '://' + host + '/' + href
+                if (href.startsWith('javascript:')) continue
+                if (href.startsWith('#')) continue
+                if (!links.includes(href)) {
+                    // Check against robots.txt
+                    links.push(href)
+                }
+            }
+        })
+        
+        console.log(proto, host, path)
+
+        console.log(await page.title())
+        console.log(await page.$('meta[name="description"]').then(e => e.getProperty('content').then(e => e.jsonValue())))
+        console.log(await page.$('meta[name="keywords"]').then(e => e.getProperty('content').then(e => e.jsonValue())).catch(e => null))
+        console.log(await page.evaluate(() => document.body.innerText.replaceAll('\n', ' ')))
+    }
+
+    let crawl = async (url) => {
+        proto = url.split(':')[0]
+        host = url.split('/')[2]
+        await robots()
+        await crawler(url)
+        console.log(score)
+        console.log(links)
+    }
+
+    crawl(process.argv[2]) // Please only put the base url here
 }
 
-urls.push(process.argv[2])
-cycle()
+engine()
