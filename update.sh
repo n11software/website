81ab18ee355faa1c7fc9e3943c7b6a36875fa771
FiRe <levih@n11.dev>
2022-11-08 18:28:54 -0600

    Add pfp dir to update script
    Signed-off-by: FiRe <levih@n11.dev>

--- update.sh
+++ update.sh
@@ -6,6 +6,7 @@ rm -r /web/n11.dev/
 mkdir /web/n11.dev/
 cp Website/website /web/n11.dev/Link
 cp -R www /web/n11.dev/www
+cp -R pfp /web/n11.dev/pfp
 cp db.txt /web/n11.dev/db.txt
 chown -R web:web /web/n11.dev/
 systemctl start n11.dev
