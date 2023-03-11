5fdcfd3954cb70094c92e157b5411f7a8a30ec12
Levi Hicks <me@levihicks.dev>
2023-03-10 20:50:52 -0600

    DB Test
    Signed-off-by: Levi Hicks <me@levihicks.dev>

--- Source/N11.cpp
+++ Source/N11.cpp
@@ -3,8 +3,9 @@
 #include <thread>
 #include <fstream>
 #include <API.hpp>
+#include <json.hpp>
 
-#define PRODUCTION false
+#define PRODUCTION true
 #define CERT "certificate.pem"
 #define KEY "key.pem"
 
@@ -26,6 +27,43 @@ std::string replace(std::string data, std::string delimiter, std::string replace
   return data;
 }
 
+bool isLoggedIn(Link::Request* request, Link::Response* response, nlohmann::json& json) {
+    Link::Request* req = new Link::Request("http://localhost/sql");
+    req->SetMethod("POST");
+    std::string id = request->GetCookie("id");
+    if (id.empty()) return false;
+    else {
+        for (int i = 0; i < id.length(); i++) {
+            switch (id[i]) {
+                case '0'...'9':
+                case 'a'...'z':
+                case 'A'...'Z':
+                    break;
+                default:
+                    return false;
+            }
+        }
+    }
+    req->SetBody("SELECT Tokens.Account.*, Tokens.Active, Tokens.Time, IP, Location, UserAgent, id FROM Session WHERE id='Session:"+request->GetCookie("id")+"';");
+    req->SetRawHeader("Authorization", "Basic cm9vdDpyb290");
+    req->SetRawHeader("Accept", "application/json");
+    req->SetRawHeader("NS", "N11");
+    req->SetRawHeader("DB", "N11");
+    req->SetRawHeader("Content-Type", "application/text");
+    req->SetRawHeader("Content-Length", std::to_string(req->GetBody().length()));
+    Link::Client client(req);
+    client.SetPort(8000);
+    Link::Response* res = client.Send();
+    response->SetBody(res->GetBody());
+    json = nlohmann::json::parse(res->GetBody());
+    if (json[0]["result"].empty()) return false;
+    return true;
+}
+
+bool isLoggedIn(Link::Request* request, Link::Response* response) {
+    nlohmann::json json;
+    return isLoggedIn(request, response, json);
+}
 
 int main() {
     Link::Server server;
@@ -33,9 +71,8 @@ int main() {
     server.SetStaticPages("public/");
 
     server.Get("/", [](Link::Request* request, Link::Response* response) {
-        std::string id = request->GetCookie("id");
-        if (id.empty()) {
-            response->SetStatus(301)->SetHeader("Location", "/login");
+        if (!isLoggedIn(request, response)) {
+            response->SetStatus(302)->SetHeader("Location", "/login");
             return;
         }
         std::ifstream file("pages/index.html");
@@ -44,19 +81,13 @@ int main() {
     });
 
     server.Get("/login", [](Link::Request* request, Link::Response* response) {
+        nlohmann::json json;
+        bool loggedIn = isLoggedIn(request, response, json);
         std::string id = request->GetCookie("id");
         std::ifstream file("pages/login.html");
         std::string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-        if (id.empty()) {
-            content = replace(content, "[nothidden]", "hidden");
-            content = replace(content, "[hidden]", "");
-            content = replace(content, "[acclist]", "");
-        } else {
-            content = replace(content, "[nothidden]", "");
-            content = replace(content, "[hidden]", "hidden");
-            std::string accList = "";
-            content = replace(content, "[acclist]", accList);
-        }
+        content = replace(content, "[ip]", request->GetIP());
+        content = replace(content, "[user-agent]", loggedIn?std::string(json[0]["result"][0]["Location"]):request->GetHeader("User-Agent"));
         response->SetBody(content);
     });
 
@@ -74,7 +105,7 @@ int main() {
         server.SetPort(8080);
     #endif
 
-    std::cout << "Server started!" << std::endl;
+    server.SetStartMessage("Server started on port " + std::to_string(server.GetPort()) + "!");
     server.Start();
     return 0;
 }
